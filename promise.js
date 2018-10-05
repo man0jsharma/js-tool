5d2f17e18d5a1d6d6a154996a7125c22813986dc
man0jsharma <maksymas@gmail.com>
2018-10-04 21:22:40 -0400

    Add promise js file

---
+++ promise.js
@@ -0,0 +1,17 @@
+const promise = new Promise(
+    (resolve, reject) => {
+        setTimeout(
+            () => {
+                console.log('Timer Timed out')
+                reject('Promise Done');
+            },
+            2000)
+    }
+)
+//Data base middleware
+
+promise
+    .then((value) => {console.log('Success ' + value)})
+    .catch(err => {console.log('Error ' + err)})
+
+// Client UI 
