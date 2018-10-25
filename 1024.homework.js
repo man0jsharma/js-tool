71f1bbf60fd9b3a1303fa159be01ba62148c2112
man0jsharma <maksymas@gmail.com>
2018-10-24 21:31:49 -0400

    Homwokk 10 24

---
+++ 1024.homework.js
@@ -0,0 +1,48 @@
+const library = {
+    name: 'Boston public library',
+    address: 'Chelsea',
+    owner: 'Charles',
+    books: [
+        {
+            name: 'Advanced React',
+            author: 'Mark',
+            price: '100',
+            ISBN: '90000'
+        },
+        {
+            name: 'Advanced Angular',
+            author: 'Mike',
+            price: '90',
+            ISBN: '90001'
+        },
+        {
+            name: 'Advanced Vue',
+            author: 'Miller',
+            price: '70',
+            ISBN: '90002'
+        },
+        {
+            name: 'Advanced Redux',
+            author: 'Jobs',
+            price: '80',
+            ISBN: '90003'
+        },
+        {
+            name: 'Advanced C++',
+            author: 'Miller',
+            price: '101',
+            ISBN: '90004'
+        }
+    ]
+}
+
+
+//Create a method find the cheapest book
+
+//Create a method to find the books wrote by 'Miller'
+
+//Create a method to fetch the library address + Owner name in this format 
+// {
+//     libraryAddress: 'xxx',
+//     owner : 'xxxxx'
+// }
