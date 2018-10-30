5a2c6c1d0b417815d77d0b08e75e604328b36aec
siri4480 <nsr0489@gmail.com>
2018-10-29 21:30:20 -0400

    1026

--- 1026homework.js
+++ 1026homework.js
@@ -1,14 +1,69 @@
-// Build home
+// // Build home
 
-// Cement 3
-// Bricks 1
-// Wood 2 => Door 
+// // Cement 3
+// // Bricks 1
+// // Wood 2 => Door 
 
 
+// // cementPromise.promise().then();
 
-const cementPromise = {
-    id: 1,
-    promise: () => new Promise((resolve, reject) => {
+// // int getComputedStyle(int add, int bd) {
+
+// // }
+
+// // function get (a, b) {
+
+// // }
+
+// // get()
+
+
+// // const get = function () {
+
+// // }
+
+
+// // ////////
+// // //FAT arrow 
+// // const get = (a , b , c) => {
+// //     c = a * b;
+// //     b = a * c;
+// //     return a;
+// // }
+
+// // const get = function (a, b , c) {
+// //     return a;
+// // }
+
+
+// // // const get = {
+// // //     prop: 27,
+// // //     method : function(a) {
+// // //         return this.prop;
+// // //     }
+// // // }
+
+var id =[];
+
+const buybrick = () => {
+    
+    return new Promise((resolve, reject) => {
+        id[1] =2;
+        if (true) {
+            setTimeout(() => {
+                resolve();
+            }, 2000)
+        } else {
+            setTimeout(() => {
+                reject()
+            }, 2000)
+        }
+    })
+}
+const buycement = () => {
+    
+    return new Promise((resolve, reject) => {
+        id[0] =1;
         if (true) {
             setTimeout(() => {
                 resolve();
@@ -21,42 +76,58 @@ const cementPromise = {
     })
 }
 
-// cementPromise.promise().then();
-
-// int getComputedStyle(int add, int bd) {
-
-// }
-
-function get (a, b) {
-
+const preparedoor = () => {
+    
+    return new Promise((resolve, reject) => {
+        id[3] =4;
+        if (true) {
+            setTimeout(() => {
+                resolve();
+            }, 2000)
+        } else {
+            setTimeout(() => {
+                reject()
+            }, 2000)
+        }
+    })
 }
 
-get()
-
-
-const get = function () {
-
+const buywood = ()  => {
+    return new Promise((resolve, reject) => {
+        id[2] =3;
+        if (true) {
+            setTimeout(() => {
+                resolve();
+            }, 2000)
+        } else {
+            setTimeout(() => {
+                reject()
+            }, 2000)
+        }
+    })
 }
 
 
-////////
-//FAT arrow 
-const get = (a , b , c) => {
-    c = a * b;
-    b = a * c;
-    return a;
+const buildhome = ()  => {
+    var sorted =id.sort();
+    
+    if(sorted)
+    {
+        buybrick().then(()=>{
+            buycement().then(()=>{
+                buywood().then(()=>{
+                    preparedoor().then(()=>{
+                        console.log('House built');
+                    })
+                })
+            })
+        }).catch(() => {
+            console.log('house is under construction');
+        })
+    }
+    
 }
 
-const get = function (a, b , c) {
-    return a;
-}
 
 
-// const get = {
-//     prop: 27,
-//     method : function(a) {
-//         return this.prop;
-//     }
-// }
-
-
+buildhome();
